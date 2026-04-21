39efa67076bb25315f0e02918569fcd36c79c383
S <sandesh.suresh18@gmail.com>
2026-04-21 21:47:13 +0530

    newlypushing

--- api-gateway/src/main/java/com/paypal/api_gateway/config/RateLimitConfig.java
+++ api-gateway/src/main/java/com/paypal/api_gateway/config/RateLimitConfig.java
@@ -1,4 +1,5 @@
 package com.paypal.api_gateway.config;
+import org.springframework.context.annotation.Primary;
 
 import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
 import org.springframework.context.annotation.Bean;
