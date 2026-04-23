daeb8d0770aa98017605728e5e5e7cb0087407ec
S <sandesh.suresh18@gmail.com>
2026-04-24 01:37:13 +0530

    newly

---
+++ init-db.sql
@@ -0,0 +1,9 @@
+-- init-db.sql
+CREATE DATABASE IF NOT EXISTS user_service_db;
+CREATE DATABASE IF NOT EXISTS wallet_service_db;
+
+USE user_service_db;
+-- Add any initial user_service tables/data here if needed
+
+USE wallet_service_db;
+-- Add any initial wallet_service tables/data here if needed
