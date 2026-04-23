c24ace697c3aedf87c847d223ea142867c703a9d
S <sandesh.suresh18@gmail.com>
2026-04-24 00:09:22 +0530

    mememe

--- user-service/src/main/java/com/paypal/user_service/client/WalletClient.java
+++ user-service/src/main/java/com/paypal/user_service/client/WalletClient.java
@@ -6,13 +6,9 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-<<<<<<< HEAD
 // @FeignClient(name = "wallet-service", url = "http://localhost:8088/api/v1/wallets")
 @FeignClient(name = "wallet-service", url = "${WALLET_SERVICE_URL:http://localhost:8088}/api/v1/wallets")
-=======
-// Use configuration property instead of hardcoded URL
-@FeignClient(name = "wallet-service", url = "${wallet.service.url:http://localhost:8088}/api/v1/wallets")
->>>>>>> ed1be7f58c9ac4a33d3f0888a1bd101508b2be19
+
 public interface WalletClient {
 
     @PostMapping
