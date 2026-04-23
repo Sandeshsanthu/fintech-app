0f2b75d8736e65105de371455ea8aaffae87f514
S <sandesh.suresh18@gmail.com>
2026-04-24 02:03:35 +0530

    java error fixed

--- user-service/src/main/java/com/paypal/user_service/client/WalletClient.java
+++ user-service/src/main/java/com/paypal/user_service/client/WalletClient.java
@@ -10,3 +10,5 @@ import org.springframework.web.bind.annotation.RequestBody;
 public interface WalletClient {
     @PostMapping
     WalletResponse createWallet(@RequestBody CreateWalletRequest request);
+
+}
